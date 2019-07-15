45925d4e671f45588c460f1b2377a3fffa565d22
chang <1469146908@qq.com>
2019-07-15 23:44:13 +0800

    bind two ParamNameResolver

--- src/main/java/org/apache/ibatis/reflection/ParamNameResolver.java
+++ src/main/java/org/apache/ibatis/reflection/ParamNameResolver.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection;
 
@@ -30,103 +30,130 @@ import org.apache.ibatis.session.RowBounds;
 
 public class ParamNameResolver {
 
-  private static final String GENERIC_NAME_PREFIX = "param";
+    private static final String GENERIC_NAME_PREFIX = "param";
 
-  /**
-   * <p>
-   * The key is the index and the value is the name of the parameter.<br />
-   * The name is obtained from {@link Param} if specified. When {@link Param} is not specified,
-   * the parameter index is used. Note that this index could be different from the actual index
-   * when the method has special parameters (i.e. {@link RowBounds} or {@link ResultHandler}).
-   * </p>
-   * <ul>
-   * <li>aMethod(@Param("M") int a, @Param("N") int b) -&gt; {{0, "M"}, {1, "N"}}</li>
-   * <li>aMethod(int a, int b) -&gt; {{0, "0"}, {1, "1"}}</li>
-   * <li>aMethod(int a, RowBounds rb, int b) -&gt; {{0, "0"}, {2, "1"}}</li>
-   * </ul>
-   */
-  private final SortedMap<Integer, String> names;
+    /**
+     * <p>
+     * {index,"alias"}
+     * 第一个为参数的索引，第二个为参数的别名，如果没有使用@Param 则为索引，如果使用了@Param则为别名
+     * 如果是特殊的RowBounds,ResultHandler,index跳过-> 0,ResultHandler,2,ResultHandler,4
+     * </p>
+     * <p>
+     * The key is the index and the value is the name of the parameter.<br />
+     * The name is obtained from {@link Param} if specified. When {@link Param} is not specified,
+     * the parameter index is used. Note that this index could be different from the actual index
+     * when the method has special parameters (i.e. {@link RowBounds} or {@link ResultHandler}).
+     * </p>
+     * <ul>
+     * <li>aMethod(@Param("M") int a, @Param("N") int b) -&gt; {{0, "M"}, {1, "N"}}</li>
+     * <li>aMethod(int a, int b) -&gt; {{0, "0"}, {1, "1"}}</li>
+     * <li>aMethod(int a, RowBounds rb, int b) -&gt; {{0, "0"}, {2, "1"}}</li>
+     * </ul>
+     */
+    private final SortedMap<Integer, String> names;
+    //是否使用了@Param注解
+    private boolean hasParamAnnotation;
 
-  private boolean hasParamAnnotation;
-
-  public ParamNameResolver(Configuration config, Method method) {
-    final Class<?>[] paramTypes = method.getParameterTypes();
-    final Annotation[][] paramAnnotations = method.getParameterAnnotations();
-    final SortedMap<Integer, String> map = new TreeMap<>();
-    int paramCount = paramAnnotations.length;
-    // get names from @Param annotations
-    for (int paramIndex = 0; paramIndex < paramCount; paramIndex++) {
-      if (isSpecialParameter(paramTypes[paramIndex])) {
-        // skip special parameters
-        continue;
-      }
-      String name = null;
-      for (Annotation annotation : paramAnnotations[paramIndex]) {
-        if (annotation instanceof Param) {
-          hasParamAnnotation = true;
-          name = ((Param) annotation).value();
-          break;
+    public ParamNameResolver(Configuration config, Method method) {
+        //获取所有参数类型
+        final Class<?>[] paramTypes = method.getParameterTypes();
+        //获取参数列表上的注解
+        final Annotation[][] paramAnnotations = method.getParameterAnnotations();
+        //临时参数对应map
+        final SortedMap<Integer, String> map = new TreeMap<>();
+        //注解参数的大小
+        int paramCount = paramAnnotations.length;
+        // get names from @Param annotations
+        for (int paramIndex = 0; paramIndex < paramCount; paramIndex++) {
+            //特殊类型
+            if (isSpecialParameter(paramTypes[paramIndex])) {
+                // skip special parameters
+                continue;
+            }
+            String name = null;
+            //注解类型
+            for (Annotation annotation : paramAnnotations[paramIndex]) {
+                if (annotation instanceof Param) {
+                    hasParamAnnotation = true;
+                    name = ((Param) annotation).value();
+                    break;
+                }
+            }
+            if (name == null) {
+                // @Param was not specified.
+                //采用实际名字
+                if (config.isUseActualParamName()) {
+                    name = getActualParamName(method, paramIndex);
+                }
+                //默认按照0,1,2,3,4 加入
+                if (name == null) {
+                    // use the parameter index as the name ("0", "1", ...)
+                    // gcode issue #71
+                    name = String.valueOf(map.size());
+                }
+            }
+            //加入
+            map.put(paramIndex, name);
         }
-      }
-      if (name == null) {
-        // @Param was not specified.
-        if (config.isUseActualParamName()) {
-          name = getActualParamName(method, paramIndex);
-        }
-        if (name == null) {
-          // use the parameter index as the name ("0", "1", ...)
-          // gcode issue #71
-          name = String.valueOf(map.size());
-        }
-      }
-      map.put(paramIndex, name);
+        //初始化
+        names = Collections.unmodifiableSortedMap(map);
     }
-    names = Collections.unmodifiableSortedMap(map);
-  }
 
-  private String getActualParamName(Method method, int paramIndex) {
-    return ParamNameUtil.getParamNames(method).get(paramIndex);
-  }
-
-  private static boolean isSpecialParameter(Class<?> clazz) {
-    return RowBounds.class.isAssignableFrom(clazz) || ResultHandler.class.isAssignableFrom(clazz);
-  }
-
-  /**
-   * Returns parameter names referenced by SQL providers.
-   */
-  public String[] getNames() {
-    return names.values().toArray(new String[0]);
-  }
-
-  /**
-   * <p>
-   * A single non-special parameter is returned without a name.
-   * Multiple parameters are named using the naming rule.
-   * In addition to the default names, this method also adds the generic names (param1, param2,
-   * ...).
-   * </p>
-   */
-  public Object getNamedParams(Object[] args) {
-    final int paramCount = names.size();
-    if (args == null || paramCount == 0) {
-      return null;
-    } else if (!hasParamAnnotation && paramCount == 1) {
-      return args[names.firstKey()];
-    } else {
-      final Map<String, Object> param = new ParamMap<>();
-      int i = 0;
-      for (Map.Entry<Integer, String> entry : names.entrySet()) {
-        param.put(entry.getValue(), args[entry.getKey()]);
-        // add generic param names (param1, param2, ...)
-        final String genericParamName = GENERIC_NAME_PREFIX + String.valueOf(i + 1);
-        // ensure not to overwrite parameter named with @Param
-        if (!names.containsValue(genericParamName)) {
-          param.put(genericParamName, args[entry.getKey()]);
-        }
-        i++;
-      }
-      return param;
+    private String getActualParamName(Method method, int paramIndex) {
+        return ParamNameUtil.getParamNames(method).get(paramIndex);
     }
-  }
+
+    /**
+     * 是否是特殊参数
+     * RowBounds 和ResultHandler 类型的参数进行跳过
+     *
+     * @param clazz
+     * @return
+     */
+    private static boolean isSpecialParameter(Class<?> clazz) {
+        return RowBounds.class.isAssignableFrom(clazz) || ResultHandler.class.isAssignableFrom(clazz);
+    }
+
+    /**
+     * Returns parameter names referenced by SQL providers.
+     */
+    public String[] getNames() {
+        return names.values().toArray(new String[0]);
+    }
+
+    /**
+     * <p>
+     * 实参绑定
+     * </p>
+     * <p>
+     * A single non-special parameter is returned without a name.
+     * Multiple parameters are named using the naming rule.
+     * In addition to the default names, this method also adds the generic names (param1, param2,
+     * ...).
+     * </p>
+     */
+    public Object getNamedParams(Object[] args) {
+        final int paramCount = names.size();
+        if (args == null || paramCount == 0) {
+            return null;
+        } else if (!hasParamAnnotation && paramCount == 1) {
+            //如果只有一个参数,并且没有注解。返回第一个
+            return args[names.firstKey()];
+        } else {
+            final Map<String, Object> param = new ParamMap<>();
+            int i = 0;
+            for (Map.Entry<Integer, String> entry : names.entrySet()) {
+                //参数位置和实际参数值的对应
+                param.put(entry.getValue(), args[entry.getKey()]);
+                // add generic param names (param1, param2, ...)
+                final String genericParamName = GENERIC_NAME_PREFIX + String.valueOf(i + 1);
+                // ensure not to overwrite parameter named with @Param
+                if (!names.containsValue(genericParamName)) {
+                    param.put(genericParamName, args[entry.getKey()]);
+                }
+                i++;
+            }
+            return param;
+        }
+    }
 }
